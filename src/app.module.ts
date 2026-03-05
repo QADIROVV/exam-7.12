e195ad31c772186feb3cb28a75189428ce5f8744
QADIROVV <sultanbekdzumagldiev@gmail.com>
2026-03-06 00:38:05 +0500

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
